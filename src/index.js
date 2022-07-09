f31c9c609407a857f7b623eb24c9ee826d324923
dbcxx <bamideledolapo.s@gmail.com>
2022-07-10 00:33:14 +0100

    base preparation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { ReactDOM } from "react";
 import './index.scss';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-ReactDOM.render(<App />)
+ReactDOM.render(<App/>, document.getElementById('root'));
+serviceWorker.register();
